e29d7d91a546beff390d65f7699bd6fba9053ff8
Emory Al-Imam <emory@yapstudios.com>
2023-11-01 13:22:18 -0700

    Fix iOS 16.x crash "Simultaneous accesses, but modification requires exclusive access
    Perform objectWillChange.send() on a subsequent run loop to avoid simultaneous access of the objectWillChange publisher

--- Sources/NavigationBackport/NavigationPathHolder.swift
+++ Sources/NavigationBackport/NavigationPathHolder.swift
@@ -3,7 +3,13 @@ import SwiftUI
 
 /// An object that publishes changes to the path Array it holds.
 class NavigationPathHolder: ObservableObject {
-  @Published var path: [AnyHashable]
+    var path: [AnyHashable] {
+        didSet {
+            DispatchQueue.main.async {
+                self.objectWillChange.send()
+            }
+        }
+    }
 
   init(path: [AnyHashable] = []) {
     self.path = path
